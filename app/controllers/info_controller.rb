e718514b59bdb021c53b0fc464f93baa420225ee
Vaibhav Todi <vaibhavtodi1989@gmail.com>
2015-05-17 23:02:34 +0530

    Phase 1 Complete

--- app/controllers/info_controller.rb
+++ app/controllers/info_controller.rb
@@ -1,21 +1,20 @@
 class InfoController < ApplicationController
 
-  def index
+
+  def load_data
     $sys = SysTem.where(os_family: params[:os], hostname: params[:hn]).first
-    pp $sys
-    
-    #redirect_to :action => 'user'
+    #pp $sys
+
+    puts "*****************************************************************"
+    puts " OS FAMILY ::  #{$sys.os_family}"
+    puts "*****************************************************************"
+
+    redirect_to :action => 'index'
   end
 
-  def software
-    @sft = $sys.softwares
-    #pp @sft
-
-    @sv = $sys.services
-    #pp @sv
-
-    @sft_ky = $sys.sft_keys
-    #pp @sft
+  def index
+    @win = $sys.windows
+    #pp @win
   end
 
   def hardware
@@ -27,6 +26,54 @@ class InfoController < ApplicationController
 
     @mem = $sys.memorys
     #pp @mem
+
+    @mthr_brd = $sys.mother_boards
+    #pp @mthr_brd
+
+    @proc = $sys.processors
+    #pp @proc
+
+    @ncard = $sys.network_cards
+    #pp @ncard
+
+    @part = $sys.partitions
+    #pp @part
+
+    @vcard = $sys.video_cards
+    #pp @vcard
+
+    @scard = $sys.sound_cards
+    #pp @scard
+
+    @op_drive = $sys.optical_drifes
+    #pp @op_drive
+
+    @scsi_c = $sys.scsi_cntrls
+    #pp @scsi_c
+
+    @modm = $sys.modems
+    #pp @modm
+
+    @mnt = $sys.mntrs
+    #pp @mnt
+
+    @pf = $sys.pagefiles
+    #pp @pf
+
+    @ip_add = $sys.ipadds
+    #pp @ip_add
+
+  end
+
+  def software
+    @sft = $sys.softwares
+    #pp @sft
+
+    @sv = $sys.services
+    #pp @sv
+
+    @sft_ky = $sys.sft_keys
+    #pp @sft
   end
 
   def user
